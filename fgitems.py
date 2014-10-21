579a6af0722718e796d039b89b20c768b26487f1
Rhys Edwards <anonymeese96@hotmail.co.uk>
2014-10-21 18:29:29 +0100

    Added karma to items.

--- fgitems.py
+++ fgitems.py
@@ -5,7 +5,8 @@ item_gun = {
 
     "description":
     """This is your police department issued Glock, fully loaded.""",
-    "mass": 2.00
+    "mass": 2
+    "karma": 1
 }
 
 item_badge = {
@@ -16,6 +17,7 @@ item_badge = {
     "description":
     """Your detective badge given to you by the New York Police Department.""",
     "mass": 0.5
+    "karma": 1
 }
 
 item_handcuffs = {
@@ -26,6 +28,7 @@ item_handcuffs = {
     "description":
     """Police handcuffs, use for work or pleasure. The choice is yours.""",
     "mass": 0.5
+    "karma": 1
 }
 
 item_jumper = {
@@ -35,6 +38,7 @@ item_jumper = {
 
     "description": """Male jumper found on the victims body. Maybe Dexter at the lab could find some useful information from this""",
     "mass": 0.75
+    "karma": 3
 }
 
 item_handbag = {
@@ -44,6 +48,7 @@ item_handbag = {
 
     "description": """Victims handbag taken from the crime scene.""",
     "mass": 0.75
+    "karma": -7
 }
 
 item_watch = {
@@ -53,6 +58,7 @@ item_watch = {
 
     "description": """ This watch could sell for a lot on the streets.""",
     "mass": 1
+    "karma" -5
 }
 
 item_beer = {
@@ -60,44 +66,44 @@ item_beer = {
     "name": "a beer bottle",
     "description": """ Signature bottle from the local bar.""",
     "mass": 1
+    "karma": 1
 }
 item_blood = {
     "id": "blood",
     "name": "blood",
     "description": """DNA results from the jumper found at the crime scene.""",
     "mass": 0.1
+    "karma": 0
 
 }
 
 item_carInformation = {
     "id": "car information",
-    "name": "Car Information",
+    "name": "car information",
     "description": """Car information of suspect obtained from the bar cctv footage.""",
-    "mass": 0.5
+    "mass": 0
+    "karma": 0
 
-}
-item_cctvTapes = {
-    "id": "CCTV tapes",
-    "name": "CCTV Tapes",
-    "description": """ CCTV footage from the bar on the night of the crime.""",
-    "mass": 0.5
 }
 item_alibi = {
     "id": "alibi",
     "name": "alibi",
     "description": """Alibi clears the suspect's involvement in the crime.""",
     "mass": 0
+    "karma": 0
 }
 item_proof = {
     "id": "proof of absence",
     "name": "proof of absence",
     "description": """Voicemail proving theat the suspect has been out of the country.""",
     "mass": 0
+    "karma": 0
 }
 
 item_warrant = {
     "id": "warrant",
     "name": "Warrant",
     "description": """Warrant to bring a suspect in for questioning.""",
-    "mass": 0.5
+    "mass": 0
+    "karma": 0
 }
